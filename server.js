af23872e215d6fdf33446ee65888121477eb7d05
chaitanya2611 <115576523+chaitanya2611@users.noreply.github.com>
2024-04-05 10:57:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,20 +8,25 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import adminCategoryRoutes from "./routes/adminCategoryRoutes.js";
 import cors from 'cors';
+import path from path;
+import { fileURLToPath } from url;
 
 //configure dotenv
 dotenv.config();
 
 //database config
 connectDB();
-
+const __filename =fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 //rest object
 const app = express();
+
 
 //middelwares
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, './client/build/')))
 
 //routes
 app.use("/api/v1/auth",authRoutes);
@@ -30,6 +35,9 @@ app.use("/api/v1/product", productRoutes);
 app.use("/api/v1/admin-category", adminCategoryRoutes);
 
 //rest api
+app.use("*",function(req,res){
+    res.sendFile(path.join(__dirname, "./client/build/index.html"))
+})
 app.get('/',(req,res)=>{
     res.send("<h1>Welcome to our app</h1>");
 });
