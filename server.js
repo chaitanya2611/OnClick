feed43eab38cc40a274dda74e8f1e4bdd3af2532
chaitanya2611 <115576523+chaitanya2611@users.noreply.github.com>
2024-04-06 10:48:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import adminCategoryRoutes from "./routes/adminCategoryRoutes.js";
 import cors from 'cors';
-import path from path;
+import path from "path";
 import { fileURLToPath } from url;
 
 //configure dotenv
