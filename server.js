6ee8bd62da84a00f1e29ac82e3d43cfe764856bd
chaitanya2611 <115576523+chaitanya2611@users.noreply.github.com>
2024-04-06 12:03:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,8 @@ app.use("/api/v1/admin-category", adminCategoryRoutes);
 
 //rest api
 app.use("*",function(req,res){
-    res.sendFile(path.join(__dirname, "./client/build/index.html"))
+    console.log(__dirname)
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 })
 app.get('/',(req,res)=>{
     res.send("<h1>Welcome to our app</h1>");
